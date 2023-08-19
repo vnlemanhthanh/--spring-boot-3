0150b4cf93b8d7493928b4a328d3a0f1788597fb
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-20 03:02:08 +0700

    mycoolapp

---
+++ mycoolapp/src/test/java/edu/vnlemanhthanh/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vnlemanhthanh.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
