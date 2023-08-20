6037289e78a8a29053dea9860058ae4b5f3b24ce
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-20 14:55:10 +0700

    config module

--- s3-01-spring-boot-overview/s3-0101-spring-boot-demo/src/main/java/edu/vnlemanhthanh/springboot/demo/mycoolapp/MycoolappApplication.java
+++ s3-01-spring-boot-overview/s3-0101-spring-boot-demo/src/main/java/edu/vnlemanhthanh/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,4 +1,4 @@
-package edu.vnlemanhthanh.mycoolapp;
+package edu.vnlemanhthanh.springboot.demo.mycoolapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- s3-01-spring-boot-overview/s3-0101-spring-boot-demo/src/test/java/edu/vnlemanhthanh/springboot/demo/mycoolapp/MycoolappApplicationTests.java
+++ s3-01-spring-boot-overview/s3-0101-spring-boot-demo/src/test/java/edu/vnlemanhthanh/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -1,4 +1,4 @@
-package edu.vnlemanhthanh.mycoolapp;
+package edu.vnlemanhthanh.springboot.demo.mycoolapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
