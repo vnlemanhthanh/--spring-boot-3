406a5a1e090a8fb757c0a9853d727c65825045e0
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-20 23:49:44 +0700

    spring boot properties

---
+++ s3-01-spring-boot-overview/s3-0106-properties-demo/src/main/java/edu/vnlemanhthanh/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,13 @@
+package edu.vnlemanhthanh.springboot.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication.class, args);
+	}
+
+}
---
+++ s3-01-spring-boot-overview/s3-0106-properties-demo/src/test/java/edu/vnlemanhthanh/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vnlemanhthanh.springboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
