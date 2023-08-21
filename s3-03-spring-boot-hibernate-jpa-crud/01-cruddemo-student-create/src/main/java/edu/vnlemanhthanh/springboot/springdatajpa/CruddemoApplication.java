e27a49d3cd558fd00e70a34a1b4a46bd4d5c71bb
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-22 00:41:27 +0700

    Primary key

--- s3-03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/edu/vnlemanhthanh/springboot/springdatajpa/CruddemoApplication.java
+++ s3-03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/edu/vnlemanhthanh/springboot/springdatajpa/CruddemoApplication.java
@@ -17,8 +17,25 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+			//createStudent(studentDAO);
+
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+
+		// create multiple students
+		System.out.println("Creating 3 student objects ...");
+		Student tempStudent1 = new Student("John", "Doe", "john@vnlemanhthanh.edu");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@vnlemanhthanh.edu");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@vnlemanhthanh.edu");
+
+		// save the student objects
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
