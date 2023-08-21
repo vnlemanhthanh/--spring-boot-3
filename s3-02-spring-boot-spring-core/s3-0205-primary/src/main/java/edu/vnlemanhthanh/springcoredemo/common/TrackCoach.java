8adfa01dac29ddd9a06818ae8d16bfee99011271
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-21 12:55:02 +0700

    primary

--- s3-02-spring-boot-spring-core/s3-0205-primary/src/main/java/edu/vnlemanhthanh/springcoredemo/common/TrackCoach.java
+++ s3-02-spring-boot-spring-core/s3-0205-primary/src/main/java/edu/vnlemanhthanh/springcoredemo/common/TrackCoach.java
@@ -1,8 +1,10 @@
 package edu.vnlemanhthanh.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class TrackCoach implements Coach {
 	@Override
 	public String getDailyWorkout() {
