48cc8f0b6ded632fd0cd1159d5b60a2304ccacc9
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-21 23:31:32 +0700

    hibernate jpa - setup project

---
+++ s3-03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/edu/vnlemanhthanh/springboot/springdatajpa/CruddemoApplication.java
@@ -0,0 +1,22 @@
+package edu.vnlemanhthanh.springboot.springdatajpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+
+}
---
+++ s3-03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/test/java/edu/vnlemanhthanh/springboot/springdatajpa/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vnlemanhthanh.springboot.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
