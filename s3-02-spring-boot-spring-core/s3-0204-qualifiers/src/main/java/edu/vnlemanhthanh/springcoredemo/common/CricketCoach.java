1857fe8263e3039b5d873e92c405f9ad0d16c93a
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-21 12:14:31 +0700

    module qualifiers

---
+++ s3-02-spring-boot-spring-core/s3-0204-qualifiers/src/main/java/edu/vnlemanhthanh/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,11 @@
+package edu.vnlemanhthanh.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+	@Override
+	public String getDailyWorkout() {
+		return "Practice fast bowling for 15 minutes";
+	}
+}
---
+++ s3-02-spring-boot-spring-core/s3-0204-qualifiers/src/main/java/edu/vnlemanhthanh/springcoredemo/rest/DemoController.java
@@ -0,0 +1,31 @@
+package edu.vnlemanhthanh.springcoredemo.rest;
+
+import edu.vnlemanhthanh.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a constructor for dependency injection
+
+    @Autowired
+    public void setMyCoach(Coach theCoach) {
+        this.myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
+
+
+
+
+
+
