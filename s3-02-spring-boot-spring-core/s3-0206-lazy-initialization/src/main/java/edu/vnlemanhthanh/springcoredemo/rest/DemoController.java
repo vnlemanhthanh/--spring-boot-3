0bf2d584dc763c5e36f2a1f78070e7fe124b15d0
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-21 13:02:35 +0700

    module lazy initialization

---
+++ s3-02-spring-boot-spring-core/s3-0206-lazy-initialization/src/main/java/edu/vnlemanhthanh/springcoredemo/rest/DemoController.java
@@ -0,0 +1,32 @@
+package edu.vnlemanhthanh.springcoredemo.rest;
+
+import edu.vnlemanhthanh.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a constructor for dependency injection
+
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
+
+
+
+
+
+
