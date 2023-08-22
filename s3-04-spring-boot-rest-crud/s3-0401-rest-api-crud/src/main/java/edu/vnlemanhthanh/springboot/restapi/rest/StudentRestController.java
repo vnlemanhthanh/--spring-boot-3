b744cec832bd66dc0dc311235d341472a5c903c3
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-23 00:53:15 +0700

    /api/students refactor

--- s3-04-spring-boot-rest-crud/s3-0401-rest-api-crud/src/main/java/edu/vnlemanhthanh/springboot/restapi/rest/StudentRestController.java
+++ s3-04-spring-boot-rest-crud/s3-0401-rest-api-crud/src/main/java/edu/vnlemanhthanh/springboot/restapi/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package edu.vnlemanhthanh.springboot.restapi.rest;
 
 import edu.vnlemanhthanh.springboot.restapi.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,22 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		ArrayList<Student> theStudents = new ArrayList<>();
+	private List<Student> theStudents;
+
+	// define @PostConstruct to load the student data ... only once!
+
+	@PostConstruct
+	public void loadData() {
+
+		theStudents = new ArrayList<>();
 
 		theStudents.add(new Student("Poornima", "Patel"));
 		theStudents.add(new Student("Mario", "Rossi"));
 		theStudents.add(new Student("Mary", "Smith"));
+	}
+
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 
 		return theStudents;
 	}
