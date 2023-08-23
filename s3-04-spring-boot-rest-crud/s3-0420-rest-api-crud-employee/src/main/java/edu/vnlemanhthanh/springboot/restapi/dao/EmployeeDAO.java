19f604fa4a178f3809ff260853fe3cb3df8e1158
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-24 01:41:46 +0700

    DAO add, update, delete

--- s3-04-spring-boot-rest-crud/s3-0420-rest-api-crud-employee/src/main/java/edu/vnlemanhthanh/springboot/restapi/dao/EmployeeDAO.java
+++ s3-04-spring-boot-rest-crud/s3-0420-rest-api-crud-employee/src/main/java/edu/vnlemanhthanh/springboot/restapi/dao/EmployeeDAO.java
@@ -7,4 +7,10 @@ import java.util.List;
 public interface EmployeeDAO {
 
 	List<Employee> findAll();
+
+	Employee findById(int theId);
+
+	Employee save(Employee theEmployee);
+
+	void deleteById(int theId);
 }
