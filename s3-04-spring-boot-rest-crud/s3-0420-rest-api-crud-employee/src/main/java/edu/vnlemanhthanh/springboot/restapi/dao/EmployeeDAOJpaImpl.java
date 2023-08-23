ba816c80d094a849fc3c0eb4b5e5ea0532e599de
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-24 01:42:16 +0700

    rest add, update, delete

--- s3-04-spring-boot-rest-crud/s3-0420-rest-api-crud-employee/src/main/java/edu/vnlemanhthanh/springboot/restapi/dao/EmployeeDAOJpaImpl.java
+++ s3-04-spring-boot-rest-crud/s3-0420-rest-api-crud-employee/src/main/java/edu/vnlemanhthanh/springboot/restapi/dao/EmployeeDAOJpaImpl.java
@@ -26,4 +26,22 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO {
 
 		return employees;
 	}
+
+	@Override
+	public Employee findById(int theId) {
+		Employee employee = entityManager.find(Employee.class, theId);
+		return employee;
+	}
+
+	@Override
+	public Employee save(Employee theEmployee) {
+		Employee dbEmployee = entityManager.merge(theEmployee);
+		return dbEmployee;
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		Employee theEmployee = entityManager.find(Employee.class, theId);
+		entityManager.remove(theEmployee);
+	}
 }
--- s3-04-spring-boot-rest-crud/s3-0420-rest-api-crud-employee/src/main/java/edu/vnlemanhthanh/springboot/restapi/rest/EmployeeRestController.java
+++ s3-04-spring-boot-rest-crud/s3-0420-rest-api-crud-employee/src/main/java/edu/vnlemanhthanh/springboot/restapi/rest/EmployeeRestController.java
@@ -1,25 +1,65 @@
 package edu.vnlemanhthanh.springboot.restapi.rest;
 
-import edu.vnlemanhthanh.springboot.restapi.dao.EmployeeDAO;
 import edu.vnlemanhthanh.springboot.restapi.entity.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.vnlemanhthanh.springboot.restapi.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-	private EmployeeDAO employeeDAO;
+	private EmployeeService employeeService;
 
-	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-		employeeDAO = theEmployeeDAO;
+	@Autowired
+	public EmployeeRestController(EmployeeService theEmployeeSerVice) {
+		employeeService = theEmployeeSerVice;
 	}
 
-	@GetMapping("employees")
+	@GetMapping("/employees")
 	public List<Employee> findAll() {
-		return employeeDAO.findAll();
+		return employeeService.findAll();
 	}
 
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		Employee theEmployee = employeeService.findById(employeeId);
+
+		if (theEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+		return theEmployee;
+	}
+
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		theEmployee.setId(0);
+		Employee dbEmployee = employeeService.save(theEmployee);
+
+		return dbEmployee;
+	}
+
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+
+		Employee dbEmployee = employeeService.save(theEmployee);
+
+		return dbEmployee;
+	}
+
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+
+		Employee tempEmployee = employeeService.findById(employeeId);
+
+		if (tempEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+
+		employeeService.deleteById(employeeId);
+
+		return "Deleted employee id - " + employeeId;
+
+	}
 }
