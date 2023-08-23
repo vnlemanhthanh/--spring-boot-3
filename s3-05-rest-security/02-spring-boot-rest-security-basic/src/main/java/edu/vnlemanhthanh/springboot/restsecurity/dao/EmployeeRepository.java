19b9af873ded1b36eadea771b51e84291ea883ab
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-24 04:25:43 +0700

    rest security

---
+++ s3-05-rest-security/02-spring-boot-rest-security-basic/src/main/java/edu/vnlemanhthanh/springboot/restsecurity/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package edu.vnlemanhthanh.springboot.restsecurity.dao;
+
+import edu.vnlemanhthanh.springboot.restsecurity.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
---
+++ s3-05-rest-security/02-spring-boot-rest-security-basic/src/test/java/edu/vnlemanhthanh/springboot/restsecurity/RestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vnlemanhthanh.springboot.restsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
