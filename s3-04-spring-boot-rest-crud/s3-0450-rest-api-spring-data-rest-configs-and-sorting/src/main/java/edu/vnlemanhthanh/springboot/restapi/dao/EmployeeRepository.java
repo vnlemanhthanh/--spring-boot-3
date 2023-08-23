9bccee11cc868bae510f6cca44f2370f772a8a61
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-24 04:00:03 +0700

    spring boot - rest security starter code

---
+++ s3-04-spring-boot-rest-crud/s3-0450-rest-api-spring-data-rest-configs-and-sorting/src/main/java/edu/vnlemanhthanh/springboot/restapi/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package edu.vnlemanhthanh.springboot.restapi.dao;
+
+import edu.vnlemanhthanh.springboot.restapi.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+}
---
+++ s3-05-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/edu/vnlemanhthanh/springboot/restsecurity/RestSecurityApplication.java
@@ -0,0 +1,13 @@
+package edu.vnlemanhthanh.springboot.restsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSecurityApplication.class, args);
+	}
+
+}
