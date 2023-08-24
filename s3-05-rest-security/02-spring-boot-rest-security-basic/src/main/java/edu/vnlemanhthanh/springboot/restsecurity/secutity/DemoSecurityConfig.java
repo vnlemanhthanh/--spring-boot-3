56bf2672438d97eb0998f45436e3866d8c5ca883
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-24 09:43:01 +0700

    restrict url based on role

--- s3-05-rest-security/02-spring-boot-rest-security-basic/src/main/java/edu/vnlemanhthanh/springboot/restsecurity/secutity/DemoSecurityConfig.java
+++ s3-05-rest-security/02-spring-boot-rest-security-basic/src/main/java/edu/vnlemanhthanh/springboot/restsecurity/secutity/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package edu.vnlemanhthanh.springboot.restsecurity.secutity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -32,4 +36,26 @@ public class DemoSecurityConfig {
 
 		return new InMemoryUserDetailsManager(john, mary, susan);
 	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(configurer -> configurer
+				.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole(
+						"EMPLOYEE")
+				.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole(
+						"ADMIN")
+
+		);
+
+		http.httpBasic(Customizer.withDefaults());
+
+		http.csrf(csrf -> csrf.disable());
+
+		return http.build();
+	}
+
 }
