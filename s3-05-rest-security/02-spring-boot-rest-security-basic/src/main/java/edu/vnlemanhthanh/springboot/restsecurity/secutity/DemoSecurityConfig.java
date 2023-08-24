78783e0b3ac16b7a758b674a41fde067b70cf93f
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-24 10:15:30 +0700

    JDBC authentication

--- s3-05-rest-security/02-spring-boot-rest-security-basic/src/main/java/edu/vnlemanhthanh/springboot/restsecurity/secutity/DemoSecurityConfig.java
+++ s3-05-rest-security/02-spring-boot-rest-security-basic/src/main/java/edu/vnlemanhthanh/springboot/restsecurity/secutity/DemoSecurityConfig.java
@@ -5,36 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
 	@Bean
-	public InMemoryUserDetailsManager userDetailsManager() {
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-		UserDetails john = User.builder()
-				.username("john")
-				.password("{noop}test123")
-				.roles("EMPLOYEE")
-				.build();
-
-		UserDetails mary = User.builder()
-				.username("mary")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER")
-				.build();
-
-		UserDetails susan = User.builder()
-				.username("susan")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER", "ADMIN")
-				.build();
-
-		return new InMemoryUserDetailsManager(john, mary, susan);
+		return new JdbcUserDetailsManager(dataSource);
 	}
 
 	@Bean
@@ -57,5 +40,29 @@ public class DemoSecurityConfig {
 
 		return http.build();
 	}
+
+//	@Bean
+//	public InMemoryUserDetailsManager userDetailsManager() {
+//
+//		UserDetails john = User.builder()
+//				.username("john")
+//				.password("{noop}test123")
+//				.roles("EMPLOYEE")
+//				.build();
+//
+//		UserDetails mary = User.builder()
+//				.username("mary")
+//				.password("{noop}test123")
+//				.roles("EMPLOYEE", "MANAGER")
+//				.build();
+//
+//		UserDetails susan = User.builder()
+//				.username("susan")
+//				.password("{noop}test123")
+//				.roles("EMPLOYEE", "MANAGER", "ADMIN")
+//				.build();
+//
+//		return new InMemoryUserDetailsManager(john, mary, susan);
+//	}
 
 }
