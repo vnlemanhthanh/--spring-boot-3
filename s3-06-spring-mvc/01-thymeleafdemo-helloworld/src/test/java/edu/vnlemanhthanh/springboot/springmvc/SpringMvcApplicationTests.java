231afc32e35e54bf9b8cea5ef056696a157a2b7d
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-25 02:46:23 +0700

    spring mvc

---
+++ s3-06-spring-mvc/01-thymeleafdemo-helloworld/src/test/java/edu/vnlemanhthanh/springboot/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.vnlemanhthanh.springboot.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
