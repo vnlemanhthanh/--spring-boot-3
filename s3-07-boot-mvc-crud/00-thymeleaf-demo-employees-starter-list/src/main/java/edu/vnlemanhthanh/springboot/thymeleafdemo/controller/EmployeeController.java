2b2f818dc5a113a281d3c2eaa38dd6df1f99c1ca
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-26 00:22:11 +0700

    index.html

--- s3-07-boot-mvc-crud/00-thymeleaf-demo-employees-starter-list/src/main/java/edu/vnlemanhthanh/springboot/thymeleafdemo/controller/EmployeeController.java
+++ s3-07-boot-mvc-crud/00-thymeleaf-demo-employees-starter-list/src/main/java/edu/vnlemanhthanh/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -2,13 +2,11 @@ package edu.vnlemanhthanh.springboot.thymeleafdemo.controller;
 
 import edu.vnlemanhthanh.springboot.thymeleafdemo.entity.Employee;
 import edu.vnlemanhthanh.springboot.thymeleafdemo.service.EmployeeService;
-import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -25,6 +23,8 @@ public class EmployeeController {
 
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
+
+		List<Employee> theEmployees = employeeService.findAll();
 
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
