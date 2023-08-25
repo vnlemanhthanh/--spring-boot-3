99fa7c62db1865b869e2437a4e475ae6d92024fc
Thanh LE <vnlemanhthanh@gmail.com>
2023-08-26 00:06:05 +0700

    delete @PostConstruct

--- s3-07-boot-mvc-crud/00-thymeleaf-demo-employees-starter-list/src/main/java/edu/vnlemanhthanh/springboot/thymeleafdemo/controller/EmployeeController.java
+++ s3-07-boot-mvc-crud/00-thymeleaf-demo-employees-starter-list/src/main/java/edu/vnlemanhthanh/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -14,25 +14,6 @@ import java.util.List;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	// load employee data
-
-	@PostConstruct
-	private void loadData() {
-
-		// create employees
-		Employee emp1 = new Employee("Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp2 = new Employee("Emma", "Baumgarten", "emma@luv2code.com");
-		Employee emp3 = new Employee("Avani", "Gupta", "avani@luv2code.com");
-
-		// create the list
-		theEmployees = new ArrayList<>();
-
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
-	}
-
 	// add mapping for "/list"
 
 	@GetMapping("/list")
