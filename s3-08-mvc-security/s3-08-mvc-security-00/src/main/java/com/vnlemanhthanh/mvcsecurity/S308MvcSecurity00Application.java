d97e3397153936da0feccd6f127d3b86ba7af572
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-09-10 16:39:58 +0700

    s3-08-mvc-security

---
+++ s3-08-mvc-security/s3-08-mvc-security-00/src/main/java/com/vnlemanhthanh/mvcsecurity/S308MvcSecurity00Application.java
@@ -0,0 +1,13 @@
+package com.vnlemanhthanh.mvcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S308MvcSecurity00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S308MvcSecurity00Application.class, args);
+    }
+
+}
---
+++ s3-08-mvc-security/s3-08-mvc-security-00/src/test/java/com/vnlemanhthanh/mvcsecurity/S308MvcSecurity00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnlemanhthanh.mvcsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S308MvcSecurity00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
